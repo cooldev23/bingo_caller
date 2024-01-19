29d0feb3aa728f3edf0f70c8cfc9eb1dbc6511d1
src_code_dev <scordill23@gmail.com>
2024-01-18 22:20:53 -0700

    Refactor js to work with objects

--- main.js
+++ main.js
@@ -9,100 +9,109 @@ const app = createApp({
       [
         {
           letter: 'B',
-          stillHasNumbers: true,
-          numbers: []
+          hasNumbersLeftUnpicked: true,
+          numbers: [],
+          colorClass: 'lightpink'
         },
         {
           letter: 'I',
-          stillHasNumbers: true,
-          numbers: []
+          hasNumbersLeftUnpicked: true,
+          numbers: [],
+          colorClass: 'lavender'
         },
         {
           letter: 'N',
-          stillHasNumbers: true,
-          numbers: []
+          hasNumbersLeftUnpicked: true,
+          numbers: [],
+          colorClass: 'lightyellow'
         },
         {
           letter: 'G',
-          stillHasNumbers: true,
-          numbers: []
+          hasNumbersLeftUnpicked: true,
+          numbers: [],
+          colorClass: 'lightblue'
         },
         {
           letter: 'O',
-          stillHasNumbers: true,
-          numbers: []
+          hasNumbersLeftUnpicked: true,
+          numbers: [],
+          colorClass: 'lightgreen'
         }
-      ],
-      allNumbers: [],
-      numbersCalled: [],
+      ]
     }
   },
   methods: {
     makePick() {
-      // set variables
-      let randNum = Math.floor(Math.random() * this.letters.length),
-        letter = this.letters[randNum],
-        count = this[letter].length,
-        numIndex = Math.floor(Math.random() * count),
-        number = this[letter][numIndex];
-
       // no letters, no game
-      if (!this.letters.length) {
+      if (this.haveAllNumbersBeenCalled()) {
+        this.getBallButtonText = 'Game Over';
         this.error = 'Game Over';
         return;
       }
 
-      /* if all numbers have been removed for a certain letter
-      remove it and show warning/error */
-      if (!count) {
-        this.letters.splice(randNum,1);
-        this.error = `All ${letter} numbers have been picked`;
+      // set variables
+      let letterObj = this.getLetterObject(),
+          numberObj = this.getNumberObject(letterObj);
+
+      // if all numbers have been picked for a certain letter set value and show warning/error
+      if (letterObj.numbers.every((item) => item.isPicked)) {
+        letterObj.hasNumbersLeftUnpicked = false;
+        this.error = `All ${letterObj.letter} numbers have been picked`;
         this.calledNumber = '';
         return;
       }
 
-      /* perform operations to remove number from parent letter (i.e. B12, G63),
-      and add to array */
-      this[letter].splice(numIndex,1);
-      this.calledNumber = letter + number;
-      this.numbersCalled.push(this.calledNumber);
+      // set called number, error to empty and number as picked
+      this.calledNumber = letterObj.letter + numberObj.num;
       this.error = '';
-      this.allNumbers[number-1].isPicked = true;
+      numberObj.isPicked = true;
 
       // change button text if the game has started
-      if (this.numbersCalled.length) {
-        this.getBallButtonText = 'Get BINGO Ball'
-      }
+      this.getBallButtonText = 'Get BINGO Ball'
     },
     populateData() {
-      // set letters property
-      // this.letters = ["B", "I", "N", "G", "O"];
+      // reset each letter's numbers
+      this.resetNumbers();
 
       // loop to set 15 numbers for each letter
       for (let i = 0; i < this.gameData.length; i++) {
         var total = (i + 1) * 15;
         let j = i === 0 ? 1 : i * 15 + 1;
         for (j; j <= total; j++) {
-          this.gameData[i].numbers.push(j);
+          this.gameData[i].numbers.push({num: j, isPicked: false});
         }
       }
 
       // reset other properties to initial state
       this.error = '';
-      this.numbersCalled = [];
       this.calledNumber = '';
-      this.allNumbers.forEach(function(item,index) {
-        item.isPicked = false;
-      })
+      this.getBallButtonText = 'Start Game';
     },
-    populateAllNumbers() {
-      for (let i = 1; i <= 75; i++) {
-        this.allNumbers.push({num: i, isPicked: false});
+    haveAllNumbersBeenCalled() {
+      for (const item of this.gameData) {
+        if (item.numbers.filter((obj) => !obj.isPicked).length) {
+          return false;
+        }
       }
+      return  true;
+    },
+    getLetterObject() {
+      return this.gameData[Math.floor(Math.random() * this.gameData.length)];
+    },
+    getNumberObject(obj) {
+      let availableNumbers = obj.numbers.filter((item) => !item.isPicked), 
+          count = availableNumbers.length,
+          numIndex = Math.floor(Math.random() * count);
+
+      return availableNumbers[numIndex];
+    },
+    resetNumbers() {
+      this.gameData.forEach(function(item) {
+        item.numbers = []
+      });
     }
   },
   created() {
-    this.populateAllNumbers();
     this.populateData();
   }
 });
