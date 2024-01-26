a9f9659c4759106876a35a6f455d26522f816fa2
src_code_dev <scordill23@gmail.com>
2024-01-26 12:01:26 -0700

    Update to Vue 3 syntax

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-const { createApp } = Vue;
+const { createApp, ref, onBeforeMount } = Vue;
 const app = createApp({
-  data() {
-    return {
-      error: '',
-      getBallButtonText: 'Start Game',
-      headerTitle: 'Welcome to the Bingo Parlor!',
-      specialMessage: 'Happy Birthday, Amabel!',
-      calledNumber: '',
-      gameData: 
-      [
+  setup() {
+    const error = ref(''),
+      getBallButtonText = ref('Start Game'),
+      headerTitle = ref('Welcome to the Bingo Parlor!'),
+      specialMessage = ref('Happy Birthday, Amabel!'),
+      calledNumber = '',
+      gameData = ref([
         {
           letter: 'B',
           hasNumbersLeftUnpicked: true,
@@ -54,82 +52,84 @@ const app = createApp({
             backgroundColor: 'lightgreen'
           }
         }
-      ]
-    }
-  },
-  methods: {
-    makePick() {
+      ]);
+
+    function makePick() {
       // no letters, no game
-      if (this.haveAllNumbersBeenCalled()) {
-        this.getBallButtonText = 'Game Over';
-        this.error = 'Game Over';
+      if (haveAllNumbersBeenCalled()) {
+        getBallButtonText.value = 'Game Over';
+        error.value = 'Game Over';
         return;
       }
 
       // set variables
-      let letterObj = this.getLetterObject(),
-          numberObj = this.getNumberObject(letterObj);
+      let letterObj = getLetterObject(),
+          numberObj = getNumberObject(letterObj);
 
       // if all numbers have been picked for a certain letter set value and show warning/error
       if (letterObj.numbers.every((item) => item.isPicked)) {
         letterObj.hasNumbersLeftUnpicked = false;
-        this.error = `All ${letterObj.letter} numbers have been picked`;
-        this.calledNumber = '';
+        error.value = `All ${letterObj.letter} numbers have been picked`;
+        calledNumber.value = '';
         return;
       }
 
       // set called number, error to empty and number as picked
-      this.calledNumber = letterObj.letter + numberObj.num;
-      this.error = '';
-      numberObj.isPicked = true;
+      calledNumber.value = letterObj.letter + numberObj.num;
+      error.value = '';
+      numberObj.isPicked.value = true;
 
       // change button text if the game has started
-      this.getBallButtonText = 'Get BINGO Ball'
-    },
-    populateData() {
+      getBallButtonText.value = 'Get BINGO Ball'
+    }
+    function populateData() {
       // reset each letter's numbers
-      this.resetNumbers();
+      resetNumbers();
 
       // loop to set 15 numbers for each letter
-      for (let i = 0; i < this.gameData.length; i++) {
+      for (let i = 0; i < gameData.length; i++) {
         var total = (i + 1) * 15;
         let j = i === 0 ? 1 : i * 15 + 1;
         for (j; j <= total; j++) {
-          this.gameData[i].numbers.push({num: j, isPicked: false});
+          gameData.value[i].numbers.push({num: j, isPicked: false});
         }
       }
 
       // reset other properties to initial state
-      this.error = '';
-      this.calledNumber = '';
-      this.getBallButtonText = 'Start Game';
-    },
-    haveAllNumbersBeenCalled() {
+      error.value = '';
+      calledNumber.value = '';
+      getBallButtonText.value = 'Start Game';
+    }
+    function haveAllNumbersBeenCalled() {
       for (const item of this.gameData) {
         if (item.numbers.filter((obj) => !obj.isPicked).length) {
           return false;
         }
       }
       return  true;
-    },
-    getLetterObject() {
-      return this.gameData[Math.floor(Math.random() * this.gameData.length)];
-    },
-    getNumberObject(obj) {
+    }
+    function getLetterObject() {
+      return gameData[Math.floor(Math.random() * this.gameData.length)];
+    }
+    function getNumberObject(obj) {
       let availableNumbers = obj.numbers.filter((item) => !item.isPicked), 
           count = availableNumbers.length,
           numIndex = Math.floor(Math.random() * count);
 
       return availableNumbers[numIndex];
-    },
-    resetNumbers() {
-      this.gameData.forEach(function(item) {
+    }
+    function resetNumbers() {
+      gameData.value.forEach(function(item) {
         item.numbers = []
       });
     }
-  },
-  created() {
-    this.populateData();
+    onBeforeMount(() => {
+      populateData();
+    })
+
+    return {
+      error, getBallButtonText, headerTitle, specialMessage, calledNumber, gameData, makePick, populateData
+    }
   }
 });
 app.mount('#app');
